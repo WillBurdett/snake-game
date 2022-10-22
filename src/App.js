3e5d30f0f85a22687af9b13a7b190aeb3026de46
WillBurdett <williamburdett1995@gmail.com>
2022-10-22 15:54:40 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import Snake from "./Snake";
 function App() {
+
+  const [state, setState] = useState({
+    snakeDots: [
+      [0,0],
+      [2,0]
+    ]
+  })
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game-area">
+       <Snake snakeDots={state.snakeDots}/>
     </div>
   );
 }
