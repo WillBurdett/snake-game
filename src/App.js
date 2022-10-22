0cab8c9eeda2122dab5e7a0478d614ca4cc36607
WillBurdett <williamburdett1995@gmail.com>
2022-10-22 16:36:09 +0100

    food appears [hardcoded]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Snake from "./Snake";
+import Food from "./Food";
 function App() {
 
   const [state, setState] = useState({
+    food: [6,8],
     snakeDots: [
       [0,0],
       [2,0]
@@ -12,6 +14,7 @@ function App() {
   return (
     <div className="game-area">
        <Snake snakeDots={state.snakeDots}/>
+       <Food dot={state.food}/>
     </div>
   );
 }
