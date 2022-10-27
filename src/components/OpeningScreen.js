1eee37c7016bdb742efbc3172dfbb2b04e999380
WillBurdett <williamburdett1995@gmail.com>
2022-10-27 22:44:06 +0100

    open screen styling and snake speed

--- src/components/OpeningScreen.js
+++ src/components/OpeningScreen.js
@@ -3,9 +3,9 @@ const OpeningScreen = ({handleStartGame}) => {
     return(
         <div className="game-area">
             <div class="container">  
-                <div class="start-game--btn">  
+                <div class="start-game--content">  
                     <h2 className="start-game--title">Snake</h2>
-                    <button onClick={handleStartGame}>Start game!</button>
+                    <button className="start-game--btn" onClick={handleStartGame}>Start game!</button>
                 </div>
             </div>
         </div>
