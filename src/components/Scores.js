b9e7375fd48303dd9344e199f08ab9bbce78b678
WillBurdett <williamburdett1995@gmail.com>
2022-11-16 12:32:54 +0000

    score converted to react class component

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,18 +1,19 @@
-import { useState } from "react"
+import { Component } from "react"
 
-const Scores = ({users}) => {
+class Scores extends Component {
 
+    render(){
     return (
         <div>
-            {users.map((u, i)=>{
+            {this.props.users.map((u, i)=>{
            return (
-            <div className="individual-score-container" key={i}>
+            <div className="individual-score-container" key={this.props.users}>
                 <h5 className="individual-score basic-font text-center">{i+1}. {u.username} {u.score}</h5>
             </div>
         )
         })}
         </div>
-    )
+    )}
 }
 
 export default Scores
