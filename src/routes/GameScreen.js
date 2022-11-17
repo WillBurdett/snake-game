d6886ec5d1c768cf92f97de2969d5b1c10c87bbd
WillBurdett <williamburdett1995@gmail.com>
2022-11-17 19:46:50 +0000

    functional components changed to class components

--- src/routes/GameScreen.js
+++ src/routes/GameScreen.js
@@ -2,14 +2,17 @@ import { Component } from "react"
 import GameOver from "../components/GameOver"
 import Gameplay from "../components/Gameplay"
 import Leaderboard from "../components/Leaderboard"
+import Scores from "../components/Scores"
 
 class GameScreen extends Component {
     state = ({
         isGameOver: false,
         isPlaying: true,
+        allUsers: [],
       })
 
-        handleGameOver = () => {
+      handleGameOver = () => {
+        this.forceUpdate();
         this.setState({
           isPlaying: false,
           isGameOver: true
@@ -17,18 +20,39 @@ class GameScreen extends Component {
       }
     
       handleRestart = () => {
+        this.forceUpdate();
         this.setState({
           isPlaying: true,
           isGameOver: false
         })
       }
 
+      getAllUsers = () => {
+        fetch("http://localhost:8080/leaderboard")
+        .then(response => response.json())
+        .then(users => {
+          const newUsersArray = [];
+          for (const u of users){
+            newUsersArray.push(u)
+          }
+            this.setState({allUsers: newUsersArray})
+          })
+            .then(e => console.log(this.state.allUsers))
+            .catch(error => console.error(error))
+        }
+
+        componentDidMount(){
+          this.getAllUsers();
+        }
+          
+      
+
     render (){
         return(
             <div className="content-container">
                 {this.state.isPlaying ? <Gameplay handleGameOver={this.handleGameOver} /> : null}
                 {this.state.isGameOver ? <GameOver handleRestart={this.handleRestart}/> : null}
-                <Leaderboard/>
+                <Leaderboard allUsers={this.state.allUsers}/>
             </div>
     )}
 }
