d5bae002806a5af8625e0221c86c6b9dac2df143
WillBurdett <williamburdett1995@gmail.com>
2022-11-24 13:42:46 +0000

    account screen menu button fixed

--- src/routes/AccountScreen.js
+++ src/routes/AccountScreen.js
@@ -3,8 +3,14 @@ import SignUp from "../components/SignUp"
 const AccountScreen = ({handleSignInOrUp}) => {
     return(
         <div className="game-area">
-            <SignUp/>
-            <button className="start-game--btn hover-green basic-font text-center" onClick={handleSignInOrUp}>Back to Main Menu!</button>
+            <div class="container">
+                <div class="account-screen__container">
+                
+                    <SignUp/>
+                    <button className="start-game--btn hover-green basic-font text-center" onClick={handleSignInOrUp}>Back to Main Menu!</button>
+                </div>
+
+            </div>
         </div>
     )
 }
