873d1bc2233cbf5da4d2eaf337c6c96409d2c2fa
WillBurdett <williamburdett1995@gmail.com>
2022-11-28 19:05:56 +0000

    users no sign in and their id is used to submit scores

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -8,18 +8,15 @@ const SignIn = () => {
     const onSubmit = async values => {
         await sleep(300)
         window.alert(JSON.stringify(values, 0, 2))
-        // await createUser({
-        //     username: values.username,
-        //     email: values.email,
-        //     password: values.password
-        // })
         signIn()
       }
 
     const [allPlayers, setAllPlayers] = useState([]);
+    const [userId, setUserId] = useState(0)
 
     const signIn = () => {
-        console.log("signed in successfully")
+        localStorage.setItem("id", userId)
+        console.log("signed in successfully with ID " + userId)
     }
 
     useEffect(() => {
@@ -50,6 +47,7 @@ const SignIn = () => {
     const passwordIsCorrect = input => {
         for (let i=0; i<allPlayers.length;i++){
             if (input === allPlayers[i].password){
+                setUserId(allPlayers[i].ID)
                 return true
             }
         }
